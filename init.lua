2267dfaec485419fd6b7b11e42ebe56bf8721748
mazzaronecristian <cristian.mazzarone@edu.unifi.it>
2024-01-13 12:33:13 +0100

    init.lua

---
+++ init.lua
@@ -0,0 +1,38 @@
+require("cristian")
+print("hello")
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set autoindent")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set smarttab")
+vim.cmd("set softtabstop=2")
+vim.cmd("set mouse=a")
+
+-- colorscheme: gruvbox
+require("gruvbox").setup({
+	terminal_colors = true, -- add neovim terminal colors
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		emphasis = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
