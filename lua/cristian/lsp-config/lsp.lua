b918cd5d5483888116f922b1438999eeec37dcda
mazzaronecristian <cristian.mazzarone@edu.unifi.it>
2024-01-14 10:52:48 +0100

    Cool remaps

--- lua/cristian/lsp-config/lsp.lua
+++ lua/cristian/lsp-config/lsp.lua
@@ -11,17 +11,18 @@ local on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true }
 
 
+	buf_set_keymap('n', 'gD', ":lua vim.lsp.buf.declaration()<CR>", opts)
 	buf_set_keymap("n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts)
 	buf_set_keymap("n", "H", ":lua vim.lsp.buf.hover()<CR>", opts)
 	buf_set_keymap("n", "gi", ":lua vim.lsp.buf.implementation()<CR>", opts)
 	buf_set_keymap("n", "gr", ":lua vim.lsp.buf.refences()<CR>", opts)
-	buf_set_keymap("n", "<leader>rn", ":lua vim.lsp.util.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", opts)
 	buf_set_keymap("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", opts)
 	buf_set_keymap("n", "<leader>ld", ":lua vim.diagnostic.open_float()<CR>", opts)
 	buf_set_keymap("n", "[d", ":lua vim.diagnostic.goto_prev()<CR>", opts)
 	buf_set_keymap("n", "]d", ":lua vim.diagnostic.goto_next()<CR>", opts)
 	buf_set_keymap("n", "<leader>lq", ":lua vim.diagnostic.setloclist()<CR>", opts)
-	buf_set_keymap("n", "<leader>lf", ":lua vim.lsp.buf.format()<CR>", opts)
+	buf_set_keymap("n", "<leader>f", ":lua vim.lsp.buf.format()<CR>", opts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
--- lua/cristian/remap.lua
+++ lua/cristian/remap.lua
@@ -4,9 +4,20 @@ vim.keymap.set('n', '<leader>e', ':NERDTreeToggle<CR>')
 vim.keymap.set('n', '<leader>f', ':NERDTreeFocus<CR>')
 
 --clipboard
-vim.keymap.set('n', '<leader>y', '"+y')
-vim.keymap.set('n', '<leader>yy', '"+yy')
-vim.keymap.set('n', '<leader>Y', '"+Y')
-vim.keymap.set('n', '<leader>p', '"+p')
-vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('x', '<leader>p', "\"_dP")
 
+--move line up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+
+vim.keymap.set('n', 'J', "mzJ`z")
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
+vim.keymap.set('n', 'n', "nzzzv")
+vim.keymap.set('n', 'N', "Nzzzv")
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
